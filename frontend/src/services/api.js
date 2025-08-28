4d59a1afd2aeea66bb827704492f9201f0ca8c2c
harikarthik123 <harikarthikvajravel12@gmail.com>
2025-08-28 20:57:42 +0530

    Updated backend URL and config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://to-do-app-7cw4.onrender.com",
+  baseURL: "https://to-do-app-7cw4.onrender.com/api",
 });
 
 export const registerUser = (data) => API.post("/auth/register", data);
